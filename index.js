9a31c0323f2c8c90a4ae00ff2a2ca07d5d89dbb0
Marcos Henrique <mhfozorio@gmail.com>
2021-12-13 11:45:32 -0300

    Sons adicionados e lógica javascript implementada

---
+++ index.js
@@ -0,0 +1,29 @@
+(function () {
+
+    const notas = {
+        "c": new Audio('./notas/middle_c.mp3'),
+        "d": new Audio('./notas/middle_d.mp3'),
+        "e": new Audio('./notas/middle_e.mp3'),
+        "f": new Audio('./notas/middle_f.mp3'),
+        "g": new Audio('./notas/middle_g.mp3'),
+        "a": new Audio('./notas/middle_a.mp3'),
+        "b": new Audio('./notas/middle_b.mp3'),
+        "c2": new Audio('./notas/high_c.mp3'),
+
+        "c_sharp": new Audio('./notas/mid_c_sharp.mp3'),
+        "d_sharp": new Audio('./notas/mid_d_sharp.mp3'),
+        "f_sharp": new Audio('./notas/mid_f_sharp.mp3'),
+        "g_sharp": new Audio('./notas/mid_g_sharp.mp3'),
+        "a_sharp": new Audio('./notas/mid_a_sharp.mp3'),
+
+    }
+
+    document.addEventListener('click', reproduz);
+
+    function reproduz(e) {
+        const target = e.target;
+        let nota = target.dataset.note;
+        notas[nota]?.play()
+       console.log(nota)
+    }
+})();
